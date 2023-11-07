6ea9033aa2ca6dd92ae31cb5cba5764f5625b290
Shaddy <shadybett540@gmail.com>
2023-11-07 20:39:27 +0300

    complete

---
+++ src/Body.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+export default function Body() {
+    const [data,setData] = useState(null)
+
+    useEffect(()=>{
+        fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-10-07&sortBy=publishedAt&apiKey=5e99a430081843aa81c53baae05d39c0')
+        .then((Response)=>Response.json())
+        .then((news)=>setData(news))
+        .catch((error)=>console.error("error fetching this data",error))
+
+    },[])
+  return (
+    <div>
+        {
+data ? data.articles.map((article,index)=>(
+    <p>Title:{article.title}</p>
+)) : <p>Loading...</p>
+        }
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+const items = [{name:'fertry',age:16,car:'Toyota'},{name:'mark',age:12,car:'bike'}]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <App  />
   </React.StrictMode>
 );
 
